d9967f0e6cf6ea375507a8e3b1ef17b703b481f7
agmo <mor.agnieszka@gmail.com>
2020-05-27 21:14:36 +0200

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+Experimenting with the Notifications API and the Web Push API
+
+# Links
+
+[Deep Dive into Web Push Notifications by Harit Himanshu](https://app.pluralsight.com/library/courses/web-push-notifications-deep-dive/table-of-contents)
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// Serve static files from specified directory
+app.use(express.static(__dirname));
+
+const server = app.listen(3000, () => {
+  const host = server.address().address
+  const port = server.address().port
+
+  console.log(`App is running at ${host}:${port}`)
+});
