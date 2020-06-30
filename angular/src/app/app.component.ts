0c5e62cf899569c17c0743b9f8430bee6568c922
agmo <mor.agnieszka@gmail.com>
2020-06-30 19:49:31 +0200

    Request permission to send notifications in Angular

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-pwa';
+
+  ngOnInit(): void {
+    this.requestNotificationPermissions();
+  }
+
+  requestNotificationPermissions = () => {
+    Notification.requestPermission(status => {
+      console.log('Notification Permission Status:', status);
+    });
+  }
 }
